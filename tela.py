82c57ef52c5987ea4df8a5b070c502e3567f4bf1
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-12 20:48:52 -0300

    Add files via upload

--- tela.py
+++ tela.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from create import creat_user, creat_movie
 from read import read_user, read_movie
 from delete import delete_user, delete_movie
+from update import modify_user, modify_movie
 
 janela = Tk()
 
@@ -53,7 +54,7 @@ class ApplicationX():
         self.bt_read = Button(self.frame_0, text='Read', bg="#1F4788", foreground='white', command=self.show_user)
         self.bt_read.place(relx=0.65, rely=0.25, relwidth=0.1, relheight=0.5)
 
-        self.bt_update = Button(self.frame_0, text='Update', bg="#1F4788", foreground='white')
+        self.bt_update = Button(self.frame_0, text='Update', bg="#1F4788", foreground='white', command=self.update_user)
         self.bt_update.place(relx=0.77, rely=0.25, relwidth=0.1, relheight=0.5)
 
         self.bt_delete = Button(self.frame_0, text='Delete', bg="#1F4788", foreground='white', command=self.user_delete)
@@ -81,6 +82,12 @@ class ApplicationX():
         self.lb_delete_user = Label(self.frame_1, text="DELETE", background='#19B5FE')
         self.lb_delete_user.place(relx=0.005, rely=0.65, relwidth=0.1, relheight=0.15)
 
+        self.lb_update_user = Label(self.frame_1, text="UPDATE", background='#19B5FE')
+        self.lb_update_user.place(relx=0.35, rely=0.65, relwidth=0.1, relheight=0.15)
+
+        self.lb_update1_user = Label(self.frame_1, text="NEW VALUE", background='#19B5FE')
+        self.lb_update1_user.place(relx=0.69, rely=0.65, relwidth=0.1, relheight=0.15)
+
     def inputs(self):
         self.input_id_user = Entry(self.frame_0)
         self.input_id_user.place(relx=0.11, rely=0.25, relwidth=0.1, relheight=0.5)
@@ -103,26 +110,32 @@ class ApplicationX():
         self.input_remove_id_user = Entry(self.frame_1)
         self.input_remove_id_user.place(relx=0.12, rely=0.65, relwidth=0.19, relheight=0.15)
 
+        self.input_update_user = Entry(self.frame_1)
+        self.input_update_user.place(relx=0.46, rely=0.65, relwidth=0.19, relheight=0.15)
+
+        self.input_update1_user = Entry(self.frame_1)
+        self.input_update1_user.place(relx=0.80, rely=0.65, relwidth=0.19, relheight=0.15)
+
     def lista_frame2(self):
         self.lista_cliente = ttk.Treeview(self.frame_2, height=3,
                                           columns=('col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7'))
         self.lista_cliente.heading('#0', text='')
-        self.lista_cliente.heading('#1', text='ID')
-        self.lista_cliente.heading('#2', text='Name')
-        self.lista_cliente.heading('#3', text='Age')
-        self.lista_cliente.heading('#4', text='E-mail')
-        self.lista_cliente.heading('#5', text='Plan')
-        self.lista_cliente.heading('#6', text='Type')
+        self.lista_cliente.heading('#1', text='idUsers')
+        self.lista_cliente.heading('#2', text='user_name')
+        self.lista_cliente.heading('#3', text='user_age')
+        self.lista_cliente.heading('#4', text='user_email')
+        self.lista_cliente.heading('#5', text='user_plan')
+        self.lista_cliente.heading('#6', text='user_type')
 
         self.lista_cliente.column("#0", width=1)
         self.lista_cliente.column("#1", width=10)
-        self.lista_cliente.column("#2", width=200)
+        self.lista_cliente.column("#2", width=180)
         self.lista_cliente.column("#3", width=50)
-        self.lista_cliente.column("#4", width=200)
+        self.lista_cliente.column("#4", width=180)
         self.lista_cliente.column("#5", width=70)
         self.lista_cliente.column("#6", width=50)
 
-        self.lista_cliente.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+        self.lista_cliente.place(relx=0.01, rely=0.1, relwidth=0.98, relheight=0.85)
         self.show_user()
 
     def insert_user(self):
@@ -145,4 +158,11 @@ class ApplicationX():
     def user_delete(self):
         delete_user(
             self.input_remove_id_user.get()
+        )
+
+    def update_user(self):
+        modify_user(
+            self.input_update_user.get(),
+            self.input_update1_user.get(),
+            self.input_id_user.get()
         )
--- update.py
+++ update.py
@@ -2,14 +2,14 @@ from connection import cursor, data_base
 
 
 def modify_user(x, y, z):
-    update_user = f'UPDATE movies SET {x} = "{y}" WHERE idmovies = {z}'
+    update_user = f'UPDATE users SET {x} = "{y}" WHERE idUsers = {z}'
 
     cursor.execute(update_user)
     data_base.commit()
 
 
 def modify_movie(x, y, z):
-    update_movie = f'UPDATE users SET {x} = "{y}" WHERE idUsers = {z}'
+    update_movie = f'UPDATE movies SET {x} = "{y}" WHERE idmovies = {z}'
 
     cursor.execute(update_movie)
     data_base.commit()
